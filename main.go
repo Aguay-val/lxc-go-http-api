9454da035fdbd9024c7c87a95aecf72918f4ec84
Aguay-val <baraise.valentin@gmail.com>
2020-06-12 16:25:40 +0200

    test

--- main.go
+++ main.go
@@ -211,7 +211,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8000",
+		Addr:    "0.0.0.0:8080",
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
